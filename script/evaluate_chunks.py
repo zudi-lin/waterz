fbfa38214b8a98449ef91b9ccf9ff80b7cf66132
Zudi Lin <linzudi@g.harvard.edu>
2019-04-11 21:27:51 -0400

    python3 support

--- script/evaluate_chunks.py
+++ script/evaluate_chunks.py
@@ -8,8 +8,8 @@ num_chunks = 5
 stat = waterz.initialize_stats()
 for i in range(num_chunks):
     stat = waterz.update_statistics_using_volume(stat, a[i * num_chunks: (i + 1) * num_chunks], b[i * num_chunks: (i + 1) * num_chunks])
-print waterz.compute_final_metrics(stat)
+print(waterz.compute_final_metrics(stat))
 
 a = a.astype(np.uint64)
 b = b.astype(np.uint64)
-print waterz.evaluate_total_volume(a, b)
+print(waterz.evaluate_total_volume(a, b))
